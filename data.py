7d9e28b7a32d41fc000e6de653632092cf38b96b
Hanting997 <oaht997@gmail.com>
2025-04-17 16:09:04 +0800

    Update data.py

--- data.py
+++ data.py
@@ -12,7 +12,7 @@ def download():
     if not os.path.exists(DATA_DIR):
         os.mkdir(DATA_DIR)
     if not os.path.exists(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048')):
-        www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
+        www = 'https://dataset.bj.bcebos.com/modelnet40/modelnet40_ply_hdf5_2048.zip'
         zipfile = os.path.basename(www)
         os.system('wget %s; unzip %s' % (www, zipfile))
         os.system('mv %s %s' % (zipfile[:-4], DATA_DIR))
